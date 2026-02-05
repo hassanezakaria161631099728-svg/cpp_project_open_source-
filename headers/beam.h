46f7fc19d99cf235d4310a239155473a3e402159
hassanezakaria161631099728-svg <hassane.zakaria.161631099728@gmail.com>
2026-02-05 19:20:51 +0100

    Add files via upload

--- headers/beam.h
+++ headers/beam.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "structs.h"
-#include <vector>
 
-std::vector<double> computeDeflection(const BeamData& d);
+double computeDeflection(const BeamData& d);
 
--- headers/io.h
+++ headers/io.h
@@ -4,8 +4,6 @@
 
 BeamData readInput(const std::string& filename);
 void writeOutput(const std::string& filename,
-                 const std::vector<double>& x,
-                 const std::vector<double>& def);
-void writeMatrix(const std::string& filename,
+                 double deflection,
                  const std::vector<std::vector<double>>& K);
 
